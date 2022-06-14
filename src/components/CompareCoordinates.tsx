1598481193d20bf3669aa1f7f023278142a0a850
Collin Tewalt <collin@sonicscholar.com>
2022-06-14 16:40:38 -0600

    Added selected item callback in coordinates list item.

--- src/components/CompareCoordinates.tsx
+++ src/components/CompareCoordinates.tsx
@@ -1,6 +1,7 @@
 import "./css/CompareCoordinates.css";
 import { Coordinates } from "../Coordinates";
 import { CompareCoordinatesListItem } from "./CompareCoordinatesListItem";
+import { useState } from "react";
 
 export type CompareCoordinatesProps = {
   coordinatesList: Coordinates[];
@@ -8,6 +9,12 @@ export type CompareCoordinatesProps = {
 export const CompareCoordinates = ({
   coordinatesList,
 }: CompareCoordinatesProps) => {
+  const [selectedItemId, setSelectedItemId] = useState(-1);
+
+  const handleItemClick = ({ id }: Coordinates) => {
+    setSelectedItemId(id);
+  };
+
   return (
     <div className="compareCoordinates">
       {coordinatesList.length > 0
@@ -15,8 +22,9 @@ export const CompareCoordinates = ({
             <CompareCoordinatesListItem
               key={coordinates.id}
               coordinates={coordinates}
-              isSelected={false}
+              isSelected={coordinates.id == selectedItemId}
               compareAgainstCoordinates={0}
+              itemClicked={handleItemClick}
             />
           ))
         : "Add coordinates to compare"}
--- src/components/CompareCoordinatesListItem.tsx
+++ src/components/CompareCoordinatesListItem.tsx
@@ -5,19 +5,25 @@ export type CompareCoordinatesListItemProps = {
   coordinates: Coordinates;
   isSelected: boolean;
   compareAgainstCoordinates: number;
+  itemClicked?: (coords: Coordinates) => void;
 };
 export const CompareCoordinatesListItem = ({
   coordinates,
   isSelected,
   compareAgainstCoordinates,
+  itemClicked,
 }: CompareCoordinatesListItemProps) => {
   let className = "compareCoordinatesListItem";
   if (isSelected) className += " itemSelected";
 
   const { name, latitude, longitude } = coordinates;
 
+  const handleClick = () => {
+    if (itemClicked) itemClicked(coordinates);
+  };
+
   return (
-    <div className={className}>
+    <div className={className} onClick={handleClick}>
       <div className="name">{name}</div>
       <div className="latlon">
         <div className="lat">{latitude}</div>
